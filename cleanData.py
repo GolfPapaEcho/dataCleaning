4c3ab3adadd55ee4d5cceb80e4b6085ca064eb77
Joe Hale <joe@changeresearch.com>
2023-02-08 10:53:38 -0500

    drop simplification

--- cleanData.py
+++ cleanData.py
@@ -11,20 +11,20 @@ df.head
 
 columnsdf = ['Time to Measuement/s', 'Mass/g (0Hz)', 'Mass/g(3 Hz)']
 
-df1 = df.drop(columnsdf[2], axis=1, inplace=True)
+df = df.drop(columnsdf[2], axis=1)
 
 
 # %%
-df2 = df.dropna(axis=0, inplace=True).reset_index()
+df = df.dropna(axis=0).reset_index()
 
 # %%
 #header = [columnsdf[0], columnsdf[1]]
-df2.to_csv('mh2301120Hertz.csv', index=False)
+df.to_csv('mh2301120Hertz.csv', index=False)
 
 
 # %%
 df3 = read()
-df4 = df.drop(columnsdf[1], axis=1, inplace=True)
+df4 = df.drop(columnsdf[1], axis=1, inplace=True)  # do you mean to drop on df or df3?
 df5 = df.dropna(axis=0, inplace=True).reset_index()
 # %%
 #header1 = [columnsdf[0], columnsdf[2]]
